4a60b695557a755e325d37695fa2fe99347439a8
Ítalo Braga <italojbraga@gmail.com>
2021-07-27 17:23:45 -0300

    add buttons pages

--- src/components/FetchPlaylist.jsx
+++ src/components/FetchPlaylist.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import DispList from "./DispList";
 
 const FetchPlaylist = () => {
+  const [pageToken, setPageToken] = useState();
   const KEY = process.env.REACT_APP_HOST_API_KEY;
-  const fetchPlaylist = async () => {
+  const fetchPlaylist = async (key) => {
+      console.log(pageToken)
     const { data } = await axios.get(
       "https://www.googleapis.com/youtube/v3/playlistItems",
       {
@@ -14,22 +16,43 @@ const FetchPlaylist = () => {
           playlistId: "PLWieWKZeFoVSLVo0Bn5UdlUdsuz21CXcF",
           key: KEY,
           maxResults: 6,
+          pageToken: pageToken,
         },
       }
     );
     return data;
   };
-  const { data, isLoading, error } = useQuery("playlist", fetchPlaylist);
-  console.log(isLoading);
+  const { data, isLoading, error } = useQuery(
+    ["playlist", pageToken],
+    fetchPlaylist,
+  );
+
+
   return (
     <div className="container">
-        <h3>Playlist FlowPodcast</h3>
+      <h3>Playlist FlowPodcast</h3>
       {error && <div>Algo saiu errado ...</div>}
 
       {isLoading ? (
         <div>Aguardando Playlist ...</div>
       ) : (
-        <DispList data={data} />
+        <>
+          <button
+            className="btn"
+            onClick={() => setPageToken(data.prevPageToken)}
+            disabled={!data.prevPageToken}
+          >
+            Página Anterior
+          </button>
+          <button
+            className="btn"
+            onClick={() => setPageToken(data.nextPageToken)}
+            disabled={!data.nextPageToken}
+          >
+            Próxima Página
+          </button>
+          <DispList data={data} />
+        </>
       )}
     </div>
   );
